74315d5ac391d53cb119f17cecfa09e2e4ccc642
ryosukeYamazaki <ryamazaki@c-fo.com>
2022-07-01 03:21:37 +0000

    :+1: golang用の設定をメンテ

--- inits/50_init.el
+++ inits/50_init.el
@@ -112,14 +112,6 @@
 (use-package which-key :ensure t)
 (which-key-mode)
 
-;; plantUML用の設定
-;; (setq plantuml-jar-path "/usr/local/Cellar/plantuml/1.2017.13/libexec/plantuml.jar")
-
-;; 非同期実行した結果は表示しない。rubocopが走ったりしてwindowsがぽこぽこ立ち上がるのを防ぐ。
-;; (add-to-list
-;;  'display-buffer-alist
-;;  (cons "\\*Async Shell Command\\*.*" (cons #'display-buffer-no-window nil)))
-
 (use-package company
   :ensure t
   :config
--- inits/60_go.el
+++ inits/60_go.el
@@ -5,19 +5,9 @@
 (use-package go-mode :ensure t)
 
 ;; 諸々の有効化、設定
-(add-hook 'go-mode-hook 'company-mode)
-(add-hook 'go-mode-hook 'flycheck-mode)
-(add-hook 'go-mode-hook 'go-eldoc-setup)
-(add-hook 'go-mode-hook #'go-guru-hl-identifier-mode)
-(set-face-attribute 'eldoc-highlight-function-argument nil
-                    :underline t :foreground "green"
-                    :weight 'bold)
 (add-hook 'go-mode-hook (lambda()
                           (setq gofmt-command "goimports")
                           (add-hook 'before-save-hook 'gofmt-before-save)
-                          (local-set-key (kbd "M-.") 'godef-jump)
-                          (local-set-key (kbd "M-,") 'pop-tag-mark)
-                          (company-mode)
-                          ;(setq indent-tabs-mode nil)    ; タブを利用
-                          ;(setq c-basic-offset 2)        ; tabサイズを2にする
                           (setq tab-width 2)))
+(add-hook 'go-mode-hook #'lsp)
+
